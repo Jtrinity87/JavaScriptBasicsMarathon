197462a0656687c9be1f7c6612958de54d5f69bc
Jtrinity87 <mercado.jacqueline.87@gmail.com>
2020-03-02 23:34:21 -0500

    javaScript Assignment

---
+++ jscoffeeMarathon.js
@@ -0,0 +1,32 @@
+console.log ("Coffee Please!");
+
+let coffeeType ="Medium Roast";
+
+console.log(coffeeType);
+
+var latte = 2
+var mocha = 3
+console.log (latte + mocha);
+console.log (latte>mocha);
+console.log (latte<mocha);
+
+let sugarfree;
+alert (sugarfree)
+
+let customer1 ="Kamala Khan"; let drinkChoice ="chai"; let quantity ="4"; let foodAllergy ="false";
+console.log (`customer1  ${drinkChoice} ${quantity} ${foodAllergy}`);
+
+let chai = 2.50
+console.log (chai*quantity)
+
+chai = 2
+console.log(chai)
+
+// let total = "Your final order is" drinkChoice + " " + quantity + " " + "$" + quantity * chai;
+// alert("Your Final Order:"+ total);
+console.log ("++++++++++++++++++++++++++++++++++");
+let total =`Your final order is ${drinkChoice} worth $${quantity * chai}`
+console.log(total)
+console.log ("++++++++++++++++++++++++++++++++++");
+
+console.log (`Nice choice of ${drinkChoice} but we reccomend sprite`);
